5909cd012f492f7aa2319d825b65ccb2f22c6069
wesleyara <wes1gom@gmail.com>
2024-04-13 00:07:19 -0300

    build: adding dependencies and modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    "nuxt-icon",
+    "@nuxtjs/google-fonts",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+    "@nuxt/ui",
+  ],
+  googleFonts: {
+    families: {
+      Barlow: true,
+      Inter: true,
+      "DM Sans": true,
+    },
+  },
 })
