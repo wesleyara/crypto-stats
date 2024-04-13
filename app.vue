e25d3ddb5cd413dfb5ad50f819bf28464059750b
wesleyara <wes1gom@gmail.com>
2024-04-13 00:08:35 -0300

    feat: adding seo meta and headers

--- app.vue
+++ app.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "BitStats",
+  description: "A simple way to track your Cryptocurrency stats",
+  ogTitle: "BitStats",
+  ogDescription: "A simple way to track your Cryptocurrency stats",
+  ogImage: "https://wesleyaraujo.dev/favicon.png",
+  ogImageUrl: "https://wesleyaraujo.dev/favicon.png",
+});
+
+useHead({
+  htmlAttrs: {
+    lang: "en",
+  },
+  link: [
+    {
+      rel: "icon",
+      type: "image/svg",
+      href: "/favicon.svg",
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+@use "~/assets/css/typography.scss";
+@use "~/assets/css/globals.scss";
+</style>
