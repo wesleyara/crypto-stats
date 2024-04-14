2ac8cea753d8d98b874c9b7d1e74be9daf2e3d0b
wesleyara <wes1gom@gmail.com>
2024-04-13 22:33:07 -0300

    feat: create home frame

---
+++ components/HomeFrame.vue
@@ -0,0 +1,27 @@
+<template>
+  <section
+    id="home"
+    class="window-width mx-auto flex flex-col items-center gap-10 py-32 text-center lg:flex-row lg:items-start lg:text-start"
+  >
+    <NuxtImg
+      src="/ilustration.png"
+      alt="A crypto ilustration"
+      class="w-[300px] rounded-lg sm:w-[640px]"
+    />
+
+    <div class="flex flex-col gap-4">
+      <h2>Welcome to <span class="text-emerald-500">CryptoStats</span></h2>
+
+      <p class="text-lg">
+        Your trusted source for real-time analytics and statistics in the
+        fascinating world of <b class="text-emerald-500">cryptocurrencies</b>.
+        <br />
+        Explore the latest market data, track trends, and discover valuable
+        insights to inform your investment decisions. Whether you're just
+        starting out or a seasoned veteran in the crypto space, CryptoStats is
+        your partner in navigating this ever-evolving universe. Join us and
+        embark on this exciting journey into the future of finance.
+      </p>
+    </div>
+  </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <TheHeader />
-  <Converter />
+  <div class="flex flex-col gap-10">
+    <TheHeader />
+    <HomeFrame />
+    <Converter />
+  </div>
 </template>
